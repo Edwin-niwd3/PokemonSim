ea478c13f239f45d1f9c5cb177fcd04c4d45f258
Edwin-niwd3 <edwin.navarro@laverne.edu>
2025-09-25 18:28:36 -0700

    Individualized functions to prevent cluttering of 'chooseMove' and prevented fainted pokemon from being chosen to switch to.

--- simulation.js
+++ simulation.js
@@ -148,6 +148,8 @@ class OffensiveAI extends RandomPlayerAI {
     // also keep the packed team string the simulator uses (if needed elsewhere)
     this.team = this.teamJson;
     this.protectedFlag = false; // Flag to track if Protect was used last turn
+    this.fainted = []
+    this.legend = new Map();
     this.state = {
       player: {
         //TODO: Keep track of if we have a substitute or not
@@ -162,12 +164,19 @@ class OffensiveAI extends RandomPlayerAI {
       },
       turn: 0,
     };
-  }
 
+    //Setup legend
+    let key = 1
+    for (const mon of this.teamJson) {
+      this.legend.set(mon.species, key);
+      this.fainted.push(false)
+      key++;
+    }
+
+  }
+    //TOOLS-----------------------------------------------------------------------------
     chooseMove(request) {
     /** TODO:
-     * Implement a more robust attack strategy, consider type advantages and stab
-     * Add in switching when facing an opponent that has a type advantage move against us
      * Implement status moves like Toxic to wear down bulky opponents
      * Consider using Priority moves when low on health/going for an aggressive kill
      */
@@ -184,7 +193,96 @@ class OffensiveAI extends RandomPlayerAI {
 
     //If we know the opponent moves, check them all to make sure they can't hit us with a super effective
     if (opponentMoves) {
-      for (const moveName of opponentMoves) {
+      this.survivalPlaystyle(opponentMoves, activePokemon);
+  }
+    //Agressive strategy: Choose the move with the highest base power
+    return this.aggresivePlaystyle(moves, activePokemon);
+  }
+
+  recordMove(pokemon, move) {
+    if (!this.state.opponent.moves.has(pokemon)) {
+      this.state.opponent.moves.set(pokemon, new Set());
+    }
+
+    const knownMoves = this.state.opponent.moves.get(pokemon);
+    knownMoves.add(move);
+
+  }
+
+  updateState(chunk) {
+    const lines = chunk.split('\n');
+    for (const line of lines) {
+      //Manage what turn it is
+      if(line.includes('|turn|')){
+        this.state.turn = parseInt(line.split('|')[2]);
+      }
+
+      //Detect switches
+      if (line.includes('|switch|')) {
+        this.handleSwitch(line);
+      }
+
+      //Detect moves used by the opponent
+      if (line.includes('|move|')){
+        this.handleMove(line);
+      }
+
+      if (line.includes('fnt')) {
+       this.handleFaint(line); 
+      }
+    }
+  }
+
+  handleSwitch(line) {
+    //Someone has switched pokemon, check if its the opponent
+        const parts = line.split('|');
+        const switchedPlayer = parts[2].startsWith('p1a') || parts[1].startsWith('p1b') ? 'p1' : 'p2';
+        if (switchedPlayer !== this.playerId) {
+          //clean string to only have pokemon name
+          const START_OF_NAME = 5; // Length of 'p2a: ' or 'p1a: '
+          let pokemonName = parts[2].slice(START_OF_NAME);
+          // Update the opponent's active Pokémon
+          // store unique list of seen opponent pokemon in an array
+          if (!this.state.opponent.pokemon.includes(pokemonName)) {
+            this.state.opponent.pokemon.push(pokemonName);
+          }
+          this.state.opponent.active = pokemonName;
+        }
+        else {
+          //clean string to only have pokemon name
+          const START_OF_NAME = 5; // Length of 'p2a: ' or 'p1a: '
+          let pokemonName = parts[2].slice(START_OF_NAME);
+          this.state.player.active = pokemonName;
+        }
+  }
+
+  handleMove(line) {
+    const parts = line.split('|');
+    //someone used a move, check if its the opponent
+    const movingPlayer = parts[2].startsWith('p1a') || parts[2].startsWith('p1b') ? 'p1' : 'p2';
+    if(movingPlayer !== this.playerId) {
+      const moveName = parts[3];
+      this.recordMove(this.state.opponent.active, moveName);
+    }
+  }
+
+  handleFaint(line) {
+    const parts = line.split('|');
+    //someone fainted, check if it is out pokemon
+    if (!parts[2].includes(this.playerId))
+    {
+      return;
+    }
+    //Otherwise, figure out what pokemon fainted
+    const START_OF_NAME = 5
+    const faintingPokemon = parts[2].slice(START_OF_NAME);
+    const key = this.legend.get(faintingPokemon);
+    this.fainted[key] = true
+  }
+
+  //Playstyles-----------------------------------------------------------------------------
+  survivalPlaystyle(opponentMoves, activePokemon) {
+    for (const moveName of opponentMoves) {
         const moveTypes = gens.get(generation).moves.get(moveName).type;
         const pokemonTypes = gens.get(generation).species.get(activePokemon)?.types;
         let max_effectiveness = 0;
@@ -210,70 +308,12 @@ class OffensiveAI extends RandomPlayerAI {
             }
             slot++;
           }
-          if (this.state.player.activeIndex !== swap) {
+          if (this.state.player.activeIndex !== swap && this.fainted[swap] !== true) {
             this.state.player.activeIndex = swap
             console.log("Swapping!")
             return `switch ${swap}`
           }
         }
-    }
-  }
-    //Agressive strategy: Choose the move with the highest base power
-    return this.aggresivePlaystyle(moves);
-  }
-
-  recordMove(pokemon, move) {
-    if (!this.state.opponent.moves.has(pokemon)) {
-      this.state.opponent.moves.set(pokemon, new Set());
-    }
-
-    const knownMoves = this.state.opponent.moves.get(pokemon);
-    knownMoves.add(move);
-
-  }
-
-  updateState(chunk) {
-    const lines = chunk.split('\n');
-    for (const line of lines) {
-      //Manage what turn it is
-      if(line.includes('|turn|')){
-        this.state.turn = parseInt(line.split('|')[2]);
-      }
-
-      //Detect switches
-      if (line.includes('|switch|')) {
-        //Someone has switched pokemon, check if its the opponent
-        const parts = line.split('|');
-        const switchedPlayer = parts[2].startsWith('p1a') || parts[1].startsWith('p1b') ? 'p1' : 'p2';
-        if (switchedPlayer !== this.playerId) {
-          //clean string to only have pokemon name
-          const START_OF_NAME = 5; // Length of 'p2a: ' or 'p1a: '
-          let pokemonName = parts[2].slice(START_OF_NAME);
-          // Update the opponent's active Pokémon
-          // store unique list of seen opponent pokemon in an array
-          if (!this.state.opponent.pokemon.includes(pokemonName)) {
-            this.state.opponent.pokemon.push(pokemonName);
-          }
-          this.state.opponent.active = pokemonName;
-        }
-        else {
-          //clean string to only have pokemon name
-          const START_OF_NAME = 5; // Length of 'p2a: ' or 'p1a: '
-          let pokemonName = parts[2].slice(START_OF_NAME);
-          this.state.player.active = pokemonName;
-        }
-      }
-
-      //Detect moves used by the opponent
-      if (line.includes('|move|')){
-        const parts = line.split('|');
-        //someone used a move, check if its the opponent
-        const movingPlayer = parts[2].startsWith('p1a') || parts[2].startsWith('p1b') ? 'p1' : 'p2';
-        if(movingPlayer !== this.playerId) {
-          const moveName = parts[3];
-          this.recordMove(this.state.opponent.active, moveName);
-        }
-      }
     }
   }
 
@@ -304,25 +344,31 @@ class OffensiveAI extends RandomPlayerAI {
     for (let i = 0; i < moves.length; i++) {
       const move = moves[i];
       if (move.disabled) continue;
-
       const moveData = dex.moves.get(move.id);
       const opponentData = dex.species.get(this.state.opponent.active);
       const playerData = dex.species.get(this.state.player.active);
+      let power = moveData.basePower;
 
       // Calculate modified power considering STAB and type effectiveness
-      let modifiedPower = moveData.basePower;
+      let modifiedPower = 1;
 
       modifiedPower *= gens.get(generation).types.totalEffectiveness(moveData.type, opponentData.types);
       
+      if(modifiedPower >= 2) {
+        //we have a super effective move, try to guess a switch in and go for a status move, if we have one
+        
+      }
+
       if (playerData.types.includes(moveData.type)) {
         modifiedPower *= STAB_BONUS;
       }
-      if (modifiedPower > maxPower) {
+      power *= modifiedPower;
+      if (power > maxPower) {
         maxPower = modifiedPower;
         bestMoveIndex = i + 1;
       }
     }
-
+    
     if (bestMoveIndex !== -1) {
       return `move ${bestMoveIndex}`;
     } else {
@@ -333,6 +379,8 @@ class OffensiveAI extends RandomPlayerAI {
 
 
 }
+
+const start = process.hrtime();
 
 const p1spec = {name: 'Bot 1', team: Teams.pack(team1Json)};
 const p2spec = {name: 'Bot 2', team: Teams.pack(team2Json)};
@@ -364,6 +412,11 @@ void (async () => {
       console.log("No Pokémon were identified.");
       return;
     }
+    console.log(`Status of Pokemon: `);
+    for (const mon of player.teamJson) {
+      let key = player.legend.get(mon.species)
+      console.log(`${mon.species}: ${player.fainted[key]}`)
+    }
 
     console.log(`Identified Pokémon: ${opponentPokemon.join(', ')}`);
     console.log("Known Moves:");
@@ -379,6 +432,8 @@ void (async () => {
 
   generateReport(p1, p1spec.name);
   generateReport(p2, p2spec.name);
+  const end = process.hrtime(start);
+  console.log(`Execution time: ${end[0]}s ${end[1]/1000000}ms`)
   console.log("\n-------------------------------------\n");
 
 })();
